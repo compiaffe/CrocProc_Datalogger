bde5492df4b35b46600089c1f3cfa9fd6d9fe6e2
Raffael Santi <compiaffe@web.de>
2013-02-24 12:57:03 +0000

    400kHz i2c and fucntions

--- Arduino_Datalogger.ino
+++ Arduino_Datalogger.ino
@@ -8,21 +8,26 @@
 
 
 #include <I2C.h>
+#define CP_OK 1
+#define CP_ERROR 0
 
+
+/*Barometer registers*/
 #define baro 0x60
+#define OUT_P_MSB  0x01
+#define OUT_P_CSB 0x02
+#define OUT_P_LSB 0x03
+#define CTRL_REG1 0x26 
+#define set_OST 0x02
 
 #define Period500Hz 2000
 
- char OUT_P_MSB = 0x01;
-  char OUT_P_CSB = 0x02;
-  char OUT_P_LSB = 0x03;
+#if 0
+ /*Temperature Registers - currently not needed*/
   char OUT_T_MSB = 0x04;
   char OUT_T_LSB = 0x05;  
-  char CTRL_REG1 = 0x26;  
-  char set_OST = 0x02;
-  unsigned long MSB_Data = 0; 
-  unsigned long CSB_Data = 0; 
-  unsigned long LSB_Data = 0; 
+#endif
+
 
 
 unsigned long raw = 0; //the raw input value in binary
@@ -36,9 +41,13 @@ unsigned int delay_val = 1800;
 unsigned int time = 0;
 
 
+void PrintSignedNumber(unsigned long );
+unsigned int SingleBarometerRead(unsigned long *);
+
 void setup()
 {
   I2c.begin();        // join i2c bus (address optional for master)
+   mTWBR = 12;         //set the I2C frequency to 400kHz 
   I2c.pullup(0);
   Serial.begin(115200);  // start serial for output
 }
@@ -47,31 +56,7 @@ void loop()
 {
   
  time = micros(); //store the start time of our reads
- 
- 
-  I2c.write(baro,CTRL_REG1,set_OST); //initiates a single barometer read.  
-I2c.read(baro,OUT_P_MSB,8);
-MSB_Data = I2c.receive();//these are the 8 MSB out of the total of 20-bit
-
-I2c.read(baro,OUT_P_CSB,8);
-CSB_Data = I2c.receive();//these are the 8 CSB out of a total of 20-bits
-
-I2c.read(baro,OUT_P_LSB,8);
-LSB_Data = (I2c.receive()>>4);//these are the 4 LSB of the 20-bit output. 
-//The bitmap of the value received from the barometer however places these at the 4 MSB positions of the 8-bit word received. 
-//The lower 4-bits are '0'. THus we rightshift to get rid of these.
-
-raw = ((MSB_Data<<12)|(CSB_Data<<4)|LSB_Data);   //all output data put together. 
-//Because we stick together MSB followed by CSB and then LSB we bitwise-OR the parts.
- //[b19-2 - 2's Complement][b1-0 - unsigned binary]
-
-//  Serial.print("MSB ");
-//  Serial.println(MSB_Data,BIN);
-//  Serial.print("CSB ");
-//  Serial.println(CSB_Data,BIN);
-//  Serial.print("LSB ");
-//  Serial.println(LSB_Data,BIN);
-  //Serial.println(output,BIN);
+ SingleBarometerRead(&raw);
 
 
 //32 element FIFO buffer
@@ -98,7 +83,7 @@ if(counter <=32){
   PrintSignedNumber(averaged[0]);
   }
   
-  
+ #if 0 
   /*ensure a sample rate of about 500Hz*/
   time = (micros()-time); //how much time has evolved since we started the read and
  if((Period500Hz-time)> 0){ 
@@ -109,7 +94,13 @@ if(counter <=32){
      delay(1000);  
    }  
  }
+ #endif
+ 
 }
+
+
+
+
 
 void PrintSignedNumber(unsigned long twoscomp){
  unsigned long bin_nofract;
@@ -130,5 +121,35 @@ void PrintSignedNumber(unsigned long twoscomp){
     Serial.print(".");
     Serial.println(fract,DEC);
 
+}
+
+unsigned int SingleBarometerRead(unsigned long *raw){
+
+  
+  /*Temporary output data*/
+  unsigned long MSB_Data = 0; 
+  unsigned long CSB_Data = 0; 
+  unsigned long LSB_Data = 0; 
+  
+  
+    I2c.write(baro,CTRL_REG1,set_OST); //initiates a single barometer read.  
+    I2c.read(baro,OUT_P_MSB,8);
+    MSB_Data = I2c.receive();//these are the 8 MSB out of the total of 20-bit
+    
+    I2c.read(baro,OUT_P_CSB,8);
+    CSB_Data = I2c.receive();//these are the 8 CSB out of a total of 20-bits
+    
+    I2c.read(baro,OUT_P_LSB,8);
+    LSB_Data = (I2c.receive()>>4);//these are the 4 LSB of the 20-bit output. 
+    //The bitmap of the value received from the barometer however places these at the 4 MSB positions of the 8-bit word received. 
+    //The lower 4-bits are '0'. THus we rightshift to get rid of these.
+    
+    *raw = ((MSB_Data<<12)|(CSB_Data<<4)|LSB_Data);   //all output data put together. 
+  if(raw != 0){
+      return CP_OK;
+  }else{
+    return CP_ERROR;
+  }
+  
 }
 
